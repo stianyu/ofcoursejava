cb6c79e9d2fd732d8541d5363b3226de79af74dd
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-06-08 11:15:41 +0800

    LeetCode34，在排序数组中查找元素的第一个和最后一个位置，二分查找，典型例题

--- MyLeetCode/src/BinarySearch/_34/Solution.java
+++ MyLeetCode/src/BinarySearch/_34/Solution.java
@@ -2,6 +2,18 @@ package BinarySearch._34;
 
 import java.util.Arrays;
 
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ *       给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *      你的算法时间复杂度必须是 O(log n) 级别。
+ *      如果数组中不存在目标值，返回 [-1, -1]。
+ * 示例 1:
+ *      输入: nums = [5,7,7,8,8,10], target = 8
+ *      输出: [3,4]
+ * 示例 2:
+ *      输入: nums = [5,7,7,8,8,10], target = 6
+ *      输出: [-1,-1]
+ */
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
         //假设值存在，其实不存在的情况也符合。一定要按照二分查找的标准写法来写，这样不用考虑恼人的边界问题
