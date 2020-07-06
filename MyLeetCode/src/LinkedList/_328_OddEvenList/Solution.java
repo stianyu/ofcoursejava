17a6724e4c75ab05532ac537f1cf58971472462b
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-07-06 09:58:34 +0800

    LeetCode#328,奇偶链表，对引用的深刻理解

---
+++ MyLeetCode/src/LinkedList/_328_OddEvenList/Solution.java
@@ -0,0 +1,35 @@
+package LinkedList._328_OddEvenList;
+
+import DynamicProgramming.common.ListNode;
+
+/**
+ * 328. 奇偶链表
+ *      给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ *      请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ * 示例 1:
+ *      输入: 1->2->3->4->5->NULL
+ *      输出: 1->3->5->2->4->NULL
+ * 示例 2:
+ *      输入: 2->1->3->5->6->4->7->NULL
+ *      输出: 2->3->6->7->1->5->4->NULL
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head, even = head.next;
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+
+        return head;  // 这里为什么可以返回 head? 意思是odd指向head，修改odd也会修改head吗，这是什么原理？
+    }
+}
