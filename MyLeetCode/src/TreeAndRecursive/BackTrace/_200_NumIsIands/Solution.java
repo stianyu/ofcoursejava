91de03fe079aededf90e80572fc4fa5e5b3d8440
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-07-10 11:20:20 +0800

    LeetCode#200，岛屿数量，回溯算法

---
+++ MyLeetCode/src/TreeAndRecursive/BackTrace/_200_NumIsIands/Solution.java
@@ -0,0 +1,65 @@
+package TreeAndRecursive.BackTrace._200_NumIsIands;
+
+/**
+ * 200. 岛屿数量
+ *      给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *      岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+ *      此外，你可以假设该网格的四条边均被水包围。
+ *
+ * 示例 1:
+     * 输入:
+     * [
+     * ['1','1','1','1','0'],
+     * ['1','1','0','1','0'],
+     * ['1','1','0','0','0'],
+     * ['0','0','0','0','0']
+     * ]
+     * 输出: 1
+ * 示例 2:
+     * 输入:
+     * [
+     * ['1','1','0','0','0'],
+     * ['1','1','0','0','0'],
+     * ['0','0','1','0','0'],
+     * ['0','0','0','1','1']
+     * ]
+     * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ */
+public class Solution {
+    // floodFill 算法
+    boolean[][] visited;
+    int[][] position = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int res;
+
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0) {
+            return res;
+        }
+
+        visited = new boolean[grid.length][grid[0].length];
+
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[0].length; col++) {
+                if(grid[row][col] == '1' && !visited[row][col]) {
+                    res++;
+                    dfs(grid, row, col);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] grid, int row, int col) {
+        visited[row][col] = true;
+        for(int[] pos : position) {
+            int newR = row + pos[0];
+            int newC = col + pos[1];
+            // 看似没有递归终止条件，其实在这里递归就停止了
+            if(newR >= 0 && newR < grid.length && newC >= 0 && newC < grid[0].length && !visited[newR][newC] && grid[newR][newC] == '1') {
+                dfs(grid, newR, newC);
+            }
+        }
+    }
+}
