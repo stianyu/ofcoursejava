12f65191bfc48c1c2d2ddd6722717cd415965434
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-07-11 22:57:15 +0800

    LeetCode#39，组合总和，回溯算法，组合问题

--- MyLeetCode/src/TreeAndRecursive/BackTrace/_39_CombinationSum/Solution.java
+++ MyLeetCode/src/TreeAndRecursive/BackTrace/_39_CombinationSum/Solution.java
@@ -1,5 +1,9 @@
 package TreeAndRecursive.BackTrace._39_CombinationSum;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 39. 组合总和
  *      给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
@@ -25,6 +29,33 @@ package TreeAndRecursive.BackTrace._39_CombinationSum;
  *        [3,5]
  *      ]
  */
-public class Solution {
+class Solution {
+    List<List<Integer>> res;
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        res = new LinkedList<>();
+        if(candidates == null || candidates.length == 0 || target < 0 ) {
+            return res;
+        }
+
+        Arrays.sort(candidates);
+        backTrack(candidates, 0, target, new LinkedList<>());
+
+        return res;
+    }
+
+    private void backTrack(int[] candidates, int index, int target, LinkedList<Integer> list) {
+        if(target == 0) {
+            res.add(new LinkedList<>(list));
+            return;
+        }
+
+        for(int i = index; i < candidates.length; i++) {   // i 从 index 开始
+            if(candidates[i] <= target) {
+                list.add(candidates[i]);
+                backTrack(candidates, i, target - candidates[i], list);  // 注意这里是从 i 开始，而不是 i + 1
+                list.remove(list.size() - 1);
+            }
+        }
+    }
 }
