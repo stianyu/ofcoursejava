30252c345b804799baea32fe6ec347e6cac79eaa
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-07-12 21:24:35 +0800

    LeetCode#111，树的最小深度，dfs

---
+++ MyLeetCode/src/TreeAndRecursive/Tree/_111_MinDepth/Solution.java
@@ -0,0 +1,36 @@
+package TreeAndRecursive.Tree._111_MinDepth;
+
+import TreeAndRecursive.common.TreeNode;
+
+/**
+ * 111. 二叉树的最小深度
+ *      给定一个二叉树，找出其最小深度。
+ *      最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *      给定二叉树 [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最小深度  2.
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right != null) {
+            return minDepth(root.right) + 1;
+        }
+
+        if (root.left != null && root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
