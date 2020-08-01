bc2f7e8ca57f464d3e375cd1df0ae3231f18c816
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-08-01 19:55:00 +0800

    Acwing#2，01背包，dp经典问题，二维降到一维

--- MyLeetCode/src/DynamicProgramming/Package9Question/_01_package/Solution.java
+++ MyLeetCode/src/DynamicProgramming/Package9Question/_01_package/Solution.java
@@ -52,7 +52,17 @@ public class Solution {
 
     /*
     为什么这里可以从 m 推到v[i]可以求出来？
-    因为初始化是 f[i] = 0; 而不是 f[0] = 0;
+    因为初始化是 f[i] = 0;  f[m] 表示的是是 i <= m 的时候取最大值。
+    如果最大价值 w_max 在 k 时取到，k < m;
+    f[k] = w_max;
+    f[0] = 0 -> f[v[0]] = w[0] -> ...
+
+    而 i = m 时可以看成，
+    f[m - k] = 0 -> f[m - k + v[0]] = w[o] -> ...
+
+    如果结果想取到恰好是m的时候是最大值，则
+    而不是 f[0] = 0; f[i] = -INF; f[m] 表示的是恰好是 m 的时候取最大值。
+    可以保证都是从0转移过来。
      */
     public static int solve2(int n, int m, int[] v, int[] w) {
         int[] f = new int[m+1];  // f[i][j]是看前i个物品，总体积为j的情况下，总价值是多少
