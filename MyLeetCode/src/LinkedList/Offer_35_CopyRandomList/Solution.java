5004b98cb32c010a3a4e467b96d308c2631bd458
sty_dawang@outlook.com <shengtianyu_wh@163.com>
2020-09-14 17:29:04 +0800

    LeetCode#Offer35，复杂链表的复制，链表深拷贝，使用hashmap

---
+++ MyLeetCode/src/LinkedList/Offer_35_CopyRandomList/Solution.java
@@ -0,0 +1,43 @@
+package LinkedList.Offer_35_CopyRandomList;
+
+import java.util.HashMap;
+
+/**
+ * 剑指 Offer 35. 复杂链表的复制
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ *
+ * 示例1：
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ *
+ */
+public class Solution {
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while(cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        while(cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+}
+class Node{
+    int val;
+    Node next;
+    Node random;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
